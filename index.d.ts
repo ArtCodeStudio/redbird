d688c3d6e7f8883ff9f1f6dc03866d8d1c12e06c
Pascal Garber <pascal@artandcode.studio>
2021-05-11 12:29:16 +0000

    Upgrade dependencies

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
-declare module 'redbird' {
-  class ReverseProxy {
-    register(src: string | URL, target: string, opts: any);
-  }
-  const createProxy: (opts: any) => ReverseProxy;
-  export = createProxy;
+
+declare class ReverseProxy {
+  register(src: string | URL, target: string, opts: any);
 }
+declare const createProxy: (opts: any) => ReverseProxy;
+
+export = createProxy;
